31e4127fd27cc45882e69c5a2c9cf878c074f1de
Viki-coding <vikimulhall@gmail.com>
2025-03-28 17:19:26 +0000

    Update subtotal

--- bag/contents.py
+++ bag/contents.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from products.models import Product
 
 def bag_contents(request):
+    print("Bag contents context processor called")
 
     bag_items = []
     total = 0
